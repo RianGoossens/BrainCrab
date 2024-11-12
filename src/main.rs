36d1c03fc3425f13c5306817410a377734cab7e9
Rian Goossens <riangoossens@gmail.com>
2024-11-13 00:55:22 +0100

    Simple cli interface

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 use std::fmt;
-use std::io::{self, stdin, Read};
+use std::io::{self, stdin, BufRead, Read};
+use std::path::PathBuf;
+
+use clap::builder::styling::AnsiColor;
+use clap::builder::Styles;
+use clap::{Parser, Subcommand};
 
 #[repr(u8)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -168,12 +173,73 @@ impl BFInterpreter {
     }
 }
 
-fn main() -> io::Result<()> {
-    let test_program = std::fs::read_to_string("bf_programs/squares.bf")?;
-    let tokens = tokenize_bf(&test_program);
+fn get_cli_style() -> Styles {
+    Styles::styled()
+        .header(AnsiColor::Yellow.on_default())
+        .usage(AnsiColor::Green.on_default())
+        .literal(AnsiColor::Green.on_default())
+        .placeholder(AnsiColor::Green.on_default())
+}
+
+#[derive(Parser)]
+#[command(version, about, long_about, styles=get_cli_style())]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Run a Brainfuck file.
+    Run { path: PathBuf },
+
+    /// Provides an interactive environment for executing Brainfuck code line-by-line.
+    ///
+    /// Type any Brainfuck code directly, pressing Enter after each line.
+    /// Once the code completes execution, the command prompt will return to the repl mode, allowing further inputs.
+    Repl,
+}
+
+fn run(path: PathBuf) -> io::Result<()> {
+    let script = std::fs::read_to_string(path)?;
+    let tokens = tokenize_bf(&script);
     let program = parse_bf(&tokens).expect("Invalid program");
-    println!("{program:?}");
     let mut interpreter = BFInterpreter::new();
     interpreter.run(&program);
     Ok(())
+}
+
+fn repl() -> io::Result<()> {
+    let mut interpreter = BFInterpreter::new();
+    loop {
+        let mut buffer = String::new();
+
+        {
+            let mut stdin = stdin().lock();
+            stdin
+                .read_line(&mut buffer)
+                .expect("Could not read line from stdin.");
+        }
+
+        match parse_bf(&tokenize_bf(&buffer)) {
+            Ok(program) => {
+                if program.0.is_empty() {
+                    return Ok(());
+                } else {
+                    interpreter.run(&program);
+                    println!();
+                }
+            }
+            Err(error) => println!("{error}"),
+        }
+    }
+}
+
+fn main() -> io::Result<()> {
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Run { path } => run(path),
+        Commands::Repl => repl(),
+    }
 }
