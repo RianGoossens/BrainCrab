c64b57c665c1a7d148e23412e7d1459e5370a49f
Rian Goossens <riangoossens@gmail.com>
2024-11-14 00:14:55 +0100

    Created braincrab_core and added a bf! macro to compile Brainfuck at compile time.

--- README.md
+++ README.md
@@ -9,5 +9,4 @@ A Brainfuck CLI. My long term goal is to turn this into a toolchain for a progra
 Here are my upcoming tasks.
 - Refine repl, perhaps use crossterm or even ratatui?
 - Start working on a new programming language on top of Brainfuck.
-- Split off code from main.rs into separate files for tokenizer, parser and interpreter.
-- A bf! macro that turns Brainfuck code into a runnable program at compile time. 
+- Clean up braincrab_core. Maybe I want a bf_core and bf_macros instead, so braincrab_core can be for the new programming language.
--- src/main.rs
+++ src/main.rs
@@ -1,177 +1,10 @@
-use std::fmt;
-use std::io::{self, stdin, BufRead, Read};
+use std::io::{self, stdin, BufRead};
 use std::path::PathBuf;
 
+use braincrab_core::{parse_bf, BFInterpreter};
 use clap::builder::styling::AnsiColor;
 use clap::builder::Styles;
 use clap::{Parser, Subcommand};
-
-#[repr(u8)]
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum BFToken {
-    Left,
-    Right,
-    Inc,
-    Dec,
-    Write,
-    Read,
-    BeginLoop,
-    EndLoop,
-}
-
-pub fn tokenize_bf(text: &str) -> Vec<BFToken> {
-    let mut result = vec![];
-    for char in text.chars() {
-        match char {
-            '<' => result.push(BFToken::Left),
-            '>' => result.push(BFToken::Right),
-            '+' => result.push(BFToken::Inc),
-            '-' => result.push(BFToken::Dec),
-            '.' => result.push(BFToken::Write),
-            ',' => result.push(BFToken::Read),
-            '[' => result.push(BFToken::BeginLoop),
-            ']' => result.push(BFToken::EndLoop),
-            _ => {}
-        }
-    }
-    result
-}
-
-#[derive(Debug, Clone)]
-pub enum BFTree {
-    Move(isize),
-    Add(u8),
-    Write,
-    Read,
-    Loop(Vec<BFTree>),
-}
-
-#[derive(Debug, Clone)]
-pub struct BFProgram(pub Vec<BFTree>);
-
-#[derive(Debug, Clone, Copy)]
-pub enum BFParseError {
-    UnmatchedBrackets,
-}
-
-impl fmt::Display for BFParseError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "unmatched brackets")
-    }
-}
-
-fn parse_bf_impl(tokens: &[BFToken], index: &mut usize) -> Vec<BFTree> {
-    let mut result = vec![];
-
-    while *index < tokens.len() {
-        match tokens[*index] {
-            BFToken::Left => {
-                if let Some(BFTree::Move(movement)) = result.last_mut() {
-                    *movement -= 1;
-                } else {
-                    result.push(BFTree::Move(-1));
-                }
-            }
-            BFToken::Right => {
-                if let Some(BFTree::Move(movement)) = result.last_mut() {
-                    *movement += 1;
-                } else {
-                    result.push(BFTree::Move(1));
-                }
-            }
-            BFToken::Inc => {
-                if let Some(BFTree::Add(addition)) = result.last_mut() {
-                    *addition = addition.wrapping_add(1);
-                } else {
-                    result.push(BFTree::Add(1));
-                }
-            }
-            BFToken::Dec => {
-                if let Some(BFTree::Add(addition)) = result.last_mut() {
-                    *addition = addition.wrapping_sub(1);
-                } else {
-                    result.push(BFTree::Add(255));
-                }
-            }
-            BFToken::Write => result.push(BFTree::Write),
-            BFToken::Read => result.push(BFTree::Read),
-            BFToken::BeginLoop => {
-                *index += 1;
-                let loop_body = parse_bf_impl(tokens, index);
-                result.push(BFTree::Loop(loop_body));
-            }
-            BFToken::EndLoop => {
-                break;
-            }
-        }
-        *index += 1;
-    }
-
-    result
-}
-
-pub fn parse_bf(tokens: &[BFToken]) -> Result<BFProgram, BFParseError> {
-    let mut index = 0;
-    let result = parse_bf_impl(tokens, &mut index);
-    if index != tokens.len() {
-        Err(BFParseError::UnmatchedBrackets)
-    } else {
-        Ok(BFProgram(result))
-    }
-}
-
-pub struct BFInterpreter {
-    tape: [u8; 30000],
-    pointer: usize,
-}
-
-impl Default for BFInterpreter {
-    fn default() -> Self {
-        Self {
-            tape: [0; 30000],
-            pointer: 0,
-        }
-    }
-}
-
-impl BFInterpreter {
-    pub fn new() -> Self {
-        Self::default()
-    }
-
-    pub fn run_instructions(&mut self, instructions: &[BFTree]) {
-        for tree in instructions {
-            match tree {
-                BFTree::Move(amount) => self.pointer = ((self.pointer as isize) + amount) as usize,
-                BFTree::Add(amount) => {
-                    self.tape[self.pointer] = self.tape[self.pointer].wrapping_add(*amount)
-                }
-                BFTree::Write => print!("{}", self.tape[self.pointer] as char),
-                BFTree::Read => {
-                    let mut byte = [0_u8];
-                    {
-                        let mut stdin_handle = stdin().lock();
-                        stdin_handle.read_exact(&mut byte).unwrap();
-                        if byte[0] == 13 {
-                            stdin_handle.read_exact(&mut byte).unwrap();
-                        }
-                    }
-                    self.tape[self.pointer] = byte[0];
-                }
-                BFTree::Loop(instructions) => loop {
-                    if self.tape[self.pointer] == 0 {
-                        break;
-                    }
-                    self.run_instructions(instructions);
-                },
-            }
-        }
-    }
-
-    pub fn run(&mut self, program: &BFProgram) {
-        self.run_instructions(&program.0);
-    }
-}
 
 fn get_cli_style() -> Styles {
     Styles::styled()
@@ -210,8 +43,7 @@ impl Cli {
 
     fn run(path: PathBuf) -> io::Result<()> {
         let script = std::fs::read_to_string(path)?;
-        let tokens = tokenize_bf(&script);
-        let program = parse_bf(&tokens).expect("Invalid program");
+        let program = parse_bf(&script).expect("Invalid program");
         let mut interpreter = BFInterpreter::new();
         interpreter.run(&program);
         Ok(())
@@ -229,7 +61,7 @@ impl Cli {
                     .expect("Could not read line from stdin.");
             }
 
-            match parse_bf(&tokenize_bf(&buffer)) {
+            match parse_bf(&buffer) {
                 Ok(program) => {
                     if program.0.is_empty() {
                         return Ok(());
